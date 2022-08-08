bd0b227e4c381b5c56ed9e18d7660d55eb2b13db
alextaylor <alextaylor515@gmail.com>
2022-08-08 11:57:29 -0400

    commit tree 5ad2be6dfde34dd3f5e930e19ea5dd9943cf840c

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,11 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
-    render json: students
+    filtered = students.where("? == first_name OR ? == last_name",params[:first_name],params[:last_name])
+    render json: filtered
   end
-
+  def student 
+    student = Student.find(params[:id])
+    render json: student
+  end
 end
